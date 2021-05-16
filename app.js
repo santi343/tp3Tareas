af76067ad34c8e6796096562adfd26e71ac7391e
santi343 <santiago1988quispep@gmail.com>
2021-05-16 10:38:44 -0300

    modificando switch

--- app.js
+++ app.js
@@ -7,37 +7,33 @@ switch (true) {
     case tarea === undefined:
         console.log("Atención - Tienes que pasar una acción.");    
         break;
-    
-    case tarea.toLowerCase() === "listar" || tarea.toLowerCase() === "crear" || tarea.toLowerCase() === "filtrar" :
-        switch (tarea.toLowerCase()) {
 
-            case "listar":
-                process.argv[3] === undefined
-                ? listar()
-                : console.log("Ingreso un valor demás");
-                break;
-
-            case "crear":
-                if(process.argv[3] !== undefined){
-                    const nuevaTarea={
-                        título: process.argv[3],
-                        estado: "pendiente"
-                    }
-                    guardarTarea(nuevaTarea);
-                    listar();
-                }else console.log("NO ingreso un título para la tarea!!!");
-                break;
-        
-            default:
-                if(process.argv[3] !== undefined){
-                   const filtrados=leerPorEstado(process.argv[3].toLowerCase());
-                   filtrados.length === 0
-                   ? console.log("Ningun dato encontrado con lo que ingreso!!!")
-                   : listarFiltrados(filtrados);
-                }else console.log("NO ingreso el estado por el que quiere Filtrar!!!");
-        }
+    case tarea.toLowerCase() === "listar":
+        process.argv[3] === undefined
+        ? listar()
+        : console.log("Ingreso un valor demás");
         break;
 
+    case tarea.toLowerCase() === "crear":
+        if(process.argv[3] !== undefined){
+            const nuevaTarea={
+                título: process.argv[3],
+                estado: "pendiente"
+            }
+            guardarTarea(nuevaTarea);
+            listar();
+        }else console.log("NO ingreso un título para la tarea!!!");
+        break;
+    
+    case tarea.toLowerCase() === "filtrar":
+        if(process.argv[3] !== undefined){
+           const filtrados=leerPorEstado(process.argv[3].toLowerCase());
+           filtrados.length === 0
+           ? console.log("Ningun dato encontrado con lo que ingreso!!!")
+           : listarFiltrados(filtrados);
+        }else console.log("NO ingreso el estado por el que quiere Filtrar!!!");
+        break;
+        
     default:
         console.log("No entiendo qué quieres hacer.");
 } 
